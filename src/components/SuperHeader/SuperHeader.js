f04a0427230c979dbb48f400592819f917fa7650
roman-usov <66104246+roman-usov@users.noreply.github.com>
2023-08-15 19:22:17 +0400

    styled the super header

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -13,11 +13,13 @@ const SuperHeader = () => {
       <MarketingMessage>
         Free shipping on domestic orders over $75!
       </MarketingMessage>
-      <SearchInput />
-      <HelpLink href="/help">Help</HelpLink>
-      <UnstyledButton>
-        <Icon id="shopping-bag" strokeWidth={1} />
-      </UnstyledButton>
+      <SearchWrapper>
+        <SearchInput />
+        <HelpLink href="/help">Help</HelpLink>
+        <UnstyledButton>
+          <Icon id="shopping-bag" strokeWidth={1} />
+        </UnstyledButton>
+      </SearchWrapper>
     </Wrapper>
   );
 };
@@ -26,10 +28,21 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 32px;
+`;
+
+const SearchWrapper = styled.div`
+  display: flex;
+  gap: 24px;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  line-height: 1rem;
+  padding: 12px 0;
 `;
 
 const HelpLink = styled.a`
